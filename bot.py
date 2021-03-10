d984dd4ce54fb23f558841d7922007936f48b61d
WokasWokas <68157350+WokasWokas@users.noreply.github.com>
2021-03-10 19:42:21 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
+    calculations.clear()
     calculations.logger(client.user, "Connected to the server!")
 
 @client.event
@@ -13,7 +14,7 @@ async def on_message(message):
     if message.author.name == client.user.name:
         return
     if message.content == "!time":
-        await message.channel.send(f"Time: {calculations.get_time()}")
+        await message.channel.send(f"Time: {calculations.Time()}")
     elif message.content == "!ping":
         await message.channel.send("pong")
     else:
